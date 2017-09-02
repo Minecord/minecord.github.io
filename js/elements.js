69994b35eab4f33f55384f9930bece7c6afc265d
Tis_awesomeness <tis@ratelimited.me>
2017-09-01 22:56:24 -0400

    Add files via upload

---
+++ js/elements.js
@@ -0,0 +1,6 @@
+
+$( document ).ready(function(){
+    $( "#navbar" ).load( "components/navbar.html" );
+    $( "#footer" ).load( "components/footer.html" );
+    $('.collapsible').collapsible();
+})

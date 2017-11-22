136327ebfab7cc49261214136c15bf8e02a60c18
Tisawesomeness <tis@creeperscreep.com>
2017-11-22 10:49:59 -0500

    Rewrote site, thanks MucaP

--- js/elements.js
+++
@@ -1,6 +0,0 @@
-
-$( document ).ready(function(){
-    $( "#navbar" ).load( "components/navbar.html" );
-    $( "#footer" ).load( "components/footer.html" );
-    $('.collapsible').collapsible();
-})
